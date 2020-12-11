baf7db0896e698d477f5d11f4f8d661d3612658e
Mat V <mvincent.it@gmail.com>
2020-12-12 00:35:15 +0100

    💺 Day 11

--- src/main/kotlin/yodamad/day/eleven/Puzzle.kt
+++ src/main/kotlin/yodamad/day/eleven/Puzzle.kt
@@ -1,5 +1,6 @@
 package yodamad.day.eleven
 
+import yodamad.day.eleven.DIR.*
 import yodamad.day.readFileIn
 
 val EMPTY = '.'
@@ -10,7 +11,8 @@ class Puzzle {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            Puzzle().findSeats()
+            //Puzzle().findSeats()
+            Puzzle().findFarAwaySeats()
         }
     }
 
@@ -48,12 +50,50 @@ class Puzzle {
 
         println("$occupied seats occupied")
     }
+
+    fun findFarAwaySeats() {
+        "full".readFileIn("day11").lines().forEach {
+            seats.add(it.toList())
+        }
+
+        var modif: Boolean
+        printMap(seats)
+        do {
+            modif = false
+            val nextRound = mutableListOf<List<Char>>()
+            seats.mapIndexed { lIdx, l ->
+                l.mapIndexed { index, c -> run {
+                    val newC = updateFarAwaySeat(lIdx, index)
+                    modif = modif || (newC != c)
+                    newC
+                }
+                }.toList()
+            }.toCollection(nextRound)
+            seats = nextRound
+            printMap(seats)
+        } while (modif)
+
+        val occupied = seats.map {
+            it.map {
+                    c -> if (c == OCCUPIED) 1 else 0
+            }.sum()
+        }.sum()
+
+        println("$occupied seats occupied")
+    }
     
     private fun updateSeat(lineIdx: Int, idx : Int) : Char =
         when(seats[lineIdx][idx]) {
             EMPTY -> EMPTY
             FREE -> if (checkSeat(lineIdx, idx) == 0) OCCUPIED else FREE
             else -> if (checkSeat(lineIdx, idx) >= 4) FREE else OCCUPIED
+        }
+
+    private fun updateFarAwaySeat(lineIdx: Int, idx : Int) : Char =
+        when(seats[lineIdx][idx]) {
+            EMPTY -> EMPTY
+            FREE -> if (checkSeats(lineIdx, idx) == 0) OCCUPIED else FREE
+            else -> if (checkSeats(lineIdx, idx) >= 5) FREE else OCCUPIED
         }
 
     private fun checkSeat(lineIdx: Int, idx : Int) : Int =
@@ -68,6 +108,105 @@ class Puzzle {
                     .plus(seats.isSeatOccupied(lineIdx + 1, idx - 1))
                     .plus(seats.isSeatOccupied(lineIdx + 1, idx))
                     .plus(seats.isSeatOccupied(lineIdx + 1, idx + 1))
+
+    private fun checkSeats(lineIdx: Int, idx : Int) : Int =
+        DIR.values().toList().map { checkWithDir(lineIdx, idx, it).isOccupied() }.sum()
+
+    private fun checkWithDir(lineIdx: Int, idx: Int, dir: DIR) : Char =
+        when(dir) {
+            UP -> {
+                var result = EMPTY
+                var round = lineIdx - 1
+                while (round >= 0 && result == EMPTY) {
+                    result = seats.getOrEmpty(round).getOrDefault(idx)
+                    round--
+                }
+                printSeat(lineIdx, idx, result, UP)
+                result
+            }
+            DOWN -> {
+                var result = EMPTY
+                var round = lineIdx + 1
+                while (round < seats.size && result == EMPTY) {
+                    result = seats.getOrEmpty(round).getOrDefault(idx)
+                    round++
+                }
+                printSeat(lineIdx, idx, result, DOWN)
+                result
+            }
+            LEFT -> {
+                var result = EMPTY
+                var round = idx - 1
+                while (round >= 0 && result == EMPTY) {
+                    result = seats.getOrEmpty(lineIdx).getOrDefault(round)
+                    round--
+                }
+                printSeat(lineIdx, idx, result, LEFT)
+                result
+            }
+            RIGHT -> {
+                var result = EMPTY
+                var round = idx + 1
+                while (round < seats[lineIdx].size && result == EMPTY) {
+                    result = seats.getOrEmpty(lineIdx).getOrDefault(round)
+                    round++
+                }
+                printSeat(lineIdx, idx, result, RIGHT)
+                result
+            }
+            UP_RIGHT -> {
+                var result = EMPTY
+                var roundX = idx + 1
+                var roundY = lineIdx - 1
+                while (roundY >= 0 && roundX < seats[lineIdx].size && result == EMPTY) {
+                    result = seats.getOrEmpty(roundY).getOrDefault(roundX)
+                    roundX++
+                    roundY--
+                }
+                printSeat(lineIdx, idx, result, UP_RIGHT)
+                result
+            }
+            UP_LEFT -> {
+                var result = EMPTY
+                var roundX = idx - 1
+                var roundY = lineIdx - 1
+                while (roundY >= 0 && roundX >= 0 && result == EMPTY) {
+                    result = seats.getOrEmpty(roundY).getOrDefault(roundX)
+                    roundX--
+                    roundY--
+                }
+                printSeat(lineIdx, idx, result, UP_LEFT)
+                result
+            }
+            DOWN_RIGHT -> {
+                var result = EMPTY
+                var roundX = idx + 1
+                var roundY = lineIdx + 1
+                while (roundY < seats.size && roundX < seats[lineIdx].size && result == EMPTY) {
+                    result = seats.getOrEmpty(roundY).getOrDefault(roundX)
+                    roundX++
+                    roundY++
+                }
+                printSeat(lineIdx, idx, result, DOWN_RIGHT)
+                result
+            }
+            DOWN_LEFT -> {
+                var result = EMPTY
+                var roundX = idx - 1
+                var roundY = lineIdx + 1
+                while (roundY < seats.size && roundX >= 0 && result == EMPTY) {
+                    result = seats.getOrEmpty(roundY).getOrDefault(roundX)
+                    roundX--
+                    roundY++
+                }
+                printSeat(lineIdx, idx, result, DOWN_LEFT)
+                result
+            }
+        }
+
+    private fun printSeat(lineIdx: Int, index: Int, seat: Char, dir : DIR) {
+        if (debug && lineIdx == seats.size - 3 && index == seats[0].size - 1) println("Seat is $seat in $dir")
+    }
 
     private fun printMap(elements : MutableList<List<Char>>) {
         if (debug) {
@@ -86,3 +225,5 @@ fun Char.isOccupied() = if (this == OCCUPIED) 1 else 0
 fun List<List<Char>>.isSeatOccupied(lIdx : Int, idx : Int) = this.getOrEmpty(lIdx).getOrDefault(idx).isOccupied()
 fun List<List<Char>>.getOrEmpty(idx : Int) = this.getOrElse(idx) { listOf() }
 fun List<Char>.getOrDefault(idx: Int) = this.getOrElse(idx) { EMPTY }
+
+enum class DIR { UP, DOWN, LEFT, RIGHT, UP_RIGHT, UP_LEFT, DOWN_RIGHT, DOWN_LEFT}
